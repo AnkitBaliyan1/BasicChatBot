20ed4c6a7831c3c162212f077d0fedaae3c1fb39
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-20 06:13:56 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,19 +1,4 @@
 import streamlit as st
-import openai
-
-
-
-def generate_response(user_input):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=user_input,
-        temperature=0.5,
-        max_tokens=100,
-        stop=None,
-        n=1
-    )
-    message = response.choices[0].text
-    return message
 
 
 
