d06fe92de4838b039e8ef5e1d7e8fd0f72f9e460
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-20 06:19:14 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -8,5 +8,21 @@ st.subheader('Demo chatbot')
 
 userInput = st.text_area("Enter Some Text:", height=100)
 
+# storing past history of chat.
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+
+
 if st.button("Submit"):
-    st.write("You are searching for :",userInput)
+    response = ("You are searching for :",userInput)
+
+    st.session_state.past.append(userInput)
+    st.session_state.generated.append(response)
+
+    st.write(response)
+
+
