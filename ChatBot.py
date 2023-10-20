29f44932fc099d7b732a3b368948a8cb070c1745
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-20 06:16:56 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -5,3 +5,8 @@ import streamlit as st
 
 st.title('OpenAI GPT-3 Streamlit Demo')
 st.subheader('Demo chatbot')
+
+userInput = st.text_area("Enter Some Text:", height=100)
+
+if st.button("Submit"):
+    st.write("You are searching for :",userInput)
