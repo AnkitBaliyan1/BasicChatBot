5aa1ada8f5b3f4778b1fce1db390c326cd44783d
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-20 06:11:52 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -22,28 +22,3 @@ def generate_response(user_input):
 
 st.title('OpenAI GPT-3 Streamlit Demo')
 st.subheader('Demo chatbot')
-
-#user_input = st.text_area("Enter some text:", height=150)
-
-
-# storing past history of chat.
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
-
-user_input=st.text_area("Enter some text: (Max word out:100)", height=150)
-if st.button("Submit"):
-    output = generate_response(user_input)
-    # store output 
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
